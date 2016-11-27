e3d5bb6ed395527da01c3073c100837036af15d9
valery <valery.fremaux@gmail.com>
2016-11-27 12:47:30 +0100

    Automated sync from SVN

--- lang/en/local_groupcopy.php
+++ lang/en/local_groupcopy.php
@@ -1,6 +1,20 @@
 <?php
+// This file is NOT part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// group structure copy between courses
+// Group structure copy between courses.
 $string['cancelledmsg'] = 'Cancelled. You are being redirected to course.';
 $string['cancelledmsg1'] = 'Cancelled. tou are being redirected to group management index.';
 $string['copyallgroupstructure'] = 'Copy groups and enrolments';
